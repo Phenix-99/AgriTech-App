858c52d1eb16c4ef2d762a6713c2b783365cd89c
desch7 <deschanel.tchatchi@gmail.com>
2021-12-16 17:15:53 +0100

    structure

--- force-app/main/default/lwc/projectRelated/projectRelated.js
+++ force-app/main/default/lwc/projectRelated/projectRelated.js
@@ -1,7 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import retrieveProjects from '@salesforce/apex/projectRelatedController.retrieveProjects';
+//import{getSobjectValue} from'@salesforce/apex';
+//import farmerName from '@salesforce/schema/Farmer__c.Name';
+//import projectName from '@salesforce/schema/Project__c.Name';
+
 
 export default class ProjectRelated extends LightningElement {
+    projList;
+    @api farmerId;
+
+    connectedCallback(){
+        retrieveProjects({cultivateurId:this.farmerId}).then(
+            result=>{ this.projList=result;
+
+            }
+        );
+    }
+
     viewDetailProject(){
-        
+
     }
 } 
