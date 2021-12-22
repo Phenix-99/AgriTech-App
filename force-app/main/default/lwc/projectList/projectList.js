b7d24b0f67809089b2a684ec8c3983193b18561a
ngDarkTracer <lorelkouam@gmail.com>
2021-12-22 15:37:50 +0100

    presentation

--- force-app/main/default/lwc/projectList/projectList.js
+++ force-app/main/default/lwc/projectList/projectList.js
@@ -19,6 +19,22 @@ export default class ProjectList extends NavigationMixin(LightningElement) {
     
     error = undefined;
 
+    get country() {
+        return (this.filterValues && this.filterValues.pays) ? this.filterValues.pays : '';
+    }
+
+    get region() {
+        return (this.filterValues && this.filterValues.region) ? this.filterValues.region : '';
+    }
+
+    get plant() {
+        return (this.filterValues && this.filterValues.plant) ? this.filterValues.plant : '';
+    }
+
+    get status() {
+        return (this.filterValues && this.filterValues.status) ? this.filterValues.status : '';
+    }
+
     @wire(getFarmerProjectsByHisName, { farmerName: '$farmer' })
     getFarmer({data, error}) {
         if (this.farmer) {
@@ -61,29 +77,6 @@ export default class ProjectList extends NavigationMixin(LightningElement) {
             .catch((error) => {
                 this.error = error;
             })
-        }
-    }
-
-    @wire(getFilteredProject, { country: '$filterValues.pays', region: '$filterValues.region', plant: '$filterValues.plant', status: '$filterValues.status' })
-    getFarmer({data, error}) {
-        if (data) {
-            this.projectList = data.map(project => {
-            const p = {
-                Id: project.Id,
-                farmer: project.Farmer__r.Name,
-                plant: project.Plant__r.Name,
-                tel: project.Farmer__r.Phone__c,
-                step: project.Notes__r ? project.Notes__r[0].Etape__c : '',
-                severity: project.Notes__r ? project.Notes__r[0].Severite__c : '',
-                country: project.Pays__c,
-                region: project.Region__c,
-                status: project.Status__c
-            }
-            return p;
-            })
-        } else if (error) {
-            console.log(error);
-            this.error = error;
         }
     }
 
