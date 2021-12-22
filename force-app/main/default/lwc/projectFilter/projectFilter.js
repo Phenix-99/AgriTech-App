9b7d283d2cca5beb6b8dfca9050343607839932a
kouekmau99 <kouekmau99@gmail.com>
2021-12-22 15:39:36 +0100

    last

--- force-app/main/default/lwc/projectFilter/projectFilter.js
+++ force-app/main/default/lwc/projectFilter/projectFilter.js
@@ -1,50 +1,15 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class ProjectFilter extends LightningElement {
-    
-    @track
     pays;
-
-    @track
     region;
-
-    @track
     plant;
-
-    @track
     status;
 
-    handleCountryChange(event) {
-        this.pays = event.target.value;
-    }
-
-    handleRegionChange(event) {
-        this.region = event.target.value;
-    }
-
-    handlePlantChange(event) {
-        this.plant = event.target.value;
-    }
-
-    handleStatusChange(event) {
-        this.status = event.target.checked;
-    }
-
     handleFilter() {
-        console.log({
-            pays : this.pays ? this.pays : '',
-            region : this.region ? this.region : '',
-            plant : this.plant ? this.plant : '',
-            status : this.status ? this.status : ''
-        });
-
+        console.log({pays, region, plant, status});
         this.dispatchEvent(new CustomEvent('filtered', {
-            detail: {
-                pays : this.pays ? this.pays : '',
-                region : this.region ? this.region : '',
-                plant : this.plant ? this.plant : '',
-                status : this.status ? this.status : ''
-            }
+            detail: {pays, region, plant, status}
         }));
     }
 }
