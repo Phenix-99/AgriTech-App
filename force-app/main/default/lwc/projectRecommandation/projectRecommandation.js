be4d496a010d4eea168708f306d89a1249173361
kouekmau99 <kouekmau99@gmail.com>
2022-01-04 09:51:20 +0100

    Ready

--- force-app/main/default/lwc/projectRecommandation/projectRecommandation.js
+++ force-app/main/default/lwc/projectRecommandation/projectRecommandation.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, api } from 'lwc';
 import getPlantRecom from '@salesforce/apex/ProjectController.getPlantRecom';
+import getCurrentStep from '@salesforce/apex/ProjectController.getCurrentStep';
 
 export default class ProjectRecommandation extends LightningElement {
 
@@ -9,6 +10,7 @@ export default class ProjectRecommandation extends LightningElement {
     activeValueMessage = '';
 
     @wire(getPlantRecom, {projectId: '$recordId'}) plantRecom;
+    @wire(getCurrentStep, {projectId: '$recordId'}) currentStep;
 
     handleActive(event) {
         this.activeValueMessage = `Tab with value ${event.target.value} is now active`;
