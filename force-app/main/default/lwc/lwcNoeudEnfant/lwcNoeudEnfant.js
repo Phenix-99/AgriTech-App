d20e668cd62d0a68a3e1a7f9cf5a3d43d456dbb3
ngDarkTracer <lorelkouam@gmail.com>
2022-03-25 12:18:15 +0100

    update form

--- force-app/main/default/lwc/lwcNoeudEnfant/lwcNoeudEnfant.js
+++ force-app/main/default/lwc/lwcNoeudEnfant/lwcNoeudEnfant.js
@@ -7,4 +7,18 @@ import {api, LightningElement} from 'lwc';
 export default class LwcNoeudEnfant extends LightningElement {
     @api value;
     @api level;
+    @api selectedKey;
+
+    get selectedClass(){
+        return this.selectedKey === this.value.numero ? 'selected item' : 'hovered item'
+    }
+    select(e) {
+        e.preventDefault();
+        this.dispatchEvent(new CustomEvent("select", {
+            detail: {
+                key: this.value.numero,
+                item: this.value
+            }
+        }));
+    }
 }
