8e2ae6247554cca4ecc1aedab8bb9f43b2d0738f
Phenix <mahmoudnjoya99@gmail.com>
2022-03-25 16:26:57 +0100

    Design improve

--- force-app/main/default/lwc/lwctreeform/lwctreeform.js
+++ force-app/main/default/lwc/lwctreeform/lwctreeform.js
@@ -2,14 +2,16 @@
  * Created by max on 24/03/2022.
  */
 
-import {LightningElement,track} from 'lwc';
+import {LightningElement} from 'lwc';
 
 export default class LwcTreeForm extends LightningElement {
 
-  @track formConfig;
+  formConfig;
+  libelle;
 
     onTreeItemSelected(e){
-        this.formConfig = e.detail.item.conditions
-        console.log(JSON.parse(JSON.stringify(this.formConfig)))
+        this.formConfig = e.detail.item.conditions;
+        this.libelle = e.detail.item.libelle;
+        console.log(JSON.parse(JSON.stringify(this.formConfig)));
     }
 }
