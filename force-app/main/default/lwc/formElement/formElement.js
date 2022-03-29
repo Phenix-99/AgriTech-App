c013004320992cd67b7b4738a98e4c9abc36e9ee
ngDarkTracer <lorelkouam@gmail.com>
2022-03-29 13:26:51 +0200

    disabled form

--- force-app/main/default/lwc/formElement/formElement.js
+++ force-app/main/default/lwc/formElement/formElement.js
@@ -1,33 +1,35 @@
 import { api, LightningElement } from 'lwc';
 
 export default class FormElement extends LightningElement {
+
     @api value;
+    @api isActive;
 
     get libelle() {
         return this.value.libelle.trim();
     }
 
     get isCheckbox() {
-        return this.value.codeFormat === 'CA' ? true : false;
+        return this.value.codeFormat === 'CA';
     }
 
     get isList() {
-        return this.value.codeFormat === 'LD' ? true : false;
+        return this.value.codeFormat === 'LD';
     }
 
     get isMontant() {
-        return this.value.codeFormat === 'MT' ? true : false;
+        return this.value.codeFormat === 'MT';
     }
 
     get isDate() {
-        return this.value.codeFormat === 'DA' ? true : false;
+        return this.value.codeFormat === 'DA';
     }
 
     get isSeparator() {
-        return this.value.codeFormat === 'SP' ? true : false;
+        return this.value.codeFormat === 'SP';
     }
 
     get isTaux() {
-        return this.value.codeFormat === 'TX' ? true : false;
+        return this.value.codeFormat === 'TX';
     }
 }
--- force-app/main/default/lwc/lwcFormBuilder/lwcFormBuilder.js
+++ force-app/main/default/lwc/lwcFormBuilder/lwcFormBuilder.js
@@ -3,4 +3,6 @@ import { LightningElement ,api} from 'lwc';
 export default class LwcFormBuilder extends LightningElement {
 
     @api formConfig;
+    @api isActive;
+    
 }
